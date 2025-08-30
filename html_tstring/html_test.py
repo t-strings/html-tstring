85256bf28362ebf5c785a2124cd35cf8b4cd8663
Dave <davepeck@gmail.com>
2025-08-30 08:22:17 -0700

    Test our CI

--- html_tstring/html_test.py
+++ html_tstring/html_test.py
@@ -1,2 +1,5 @@
-def test_todo():
-    assert True
+def test_t_strings():
+    # Temporary. Ensure our CI action is running the 3.14 RC
+    template = t"hello {42}"
+    assert len(template.strings) == 2
+    assert len(template.interpolations) == 1
